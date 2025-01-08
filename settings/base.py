632db65ba5a17632f0ef867af1adbbbd61a06e7c
Mohit Prajapat <mohit.prajapat@trootech.com>
2025-01-08 19:38:55 +0530

    [Setup] Settings File Setup

---
+++ settings/base.py
@@ -0,0 +1,174 @@
+from os.path import join
+from pathlib import Path
+
+from dj_database_url import parse
+from django.utils.timezone import timedelta  # noqa
+
+from utils.constants import Settings, EmailConfig, CeleryConfig
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+APPEND_SLASH = True
+
+admins = (("Mohit Prajapat", """mohit.prajapat@trootech.com"""),)
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = Settings.SECRET_KEY
+
+# Installed Apps
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+PROJECT_APPS = []
+
+# Third Party Apps
+THIRD_PARTY_APPS = []
+
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
+
+# Middlewares
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# Root Urls
+ROOT_URLCONF = Settings.ROOT_URLCONF
+
+# Templates + Context Processors
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [join(BASE_DIR, Settings.TEMPLATES)],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# WSGI - Web Server Gateway Interface Server
+WSGI_APPLICATION = Settings.WSGI_APPLICATION
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+DATABASES = {"default": parse(Settings.DATABASE_URL)}
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+LANGUAGE_CODE = Settings.LANGUAGE_CODE
+
+TIME_ZONE = Settings.TIME_ZONE
+
+USE_I18N = Settings.USE_I18N
+
+USE_TZ = Settings.USE_TZ
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = Settings.STATIC_URL
+STATICFILES_DIRS = [join(BASE_DIR, Settings.STATIC_FILES_DIRS)]
+STATIC_ROOT = join(BASE_DIR, Settings.STATIC_ROOT)
+
+# Media files (Models File)
+MEDIA_URL = Settings.MEDIA_URL
+MEDIA_ROOT = join(BASE_DIR, Settings.MEDIA_ROOT)
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# -------------------------------------------------
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Email Configuration
+EMAIL_BACKEND = EmailConfig.EMAIL_BACKEND
+EMAIL_HOST = EmailConfig.EMAIL_HOST
+EMAIL_USE_SSL = True  # use port 465
+EMAIL_USE_TLS = False  # use port 587
+EMAIL_PORT = EmailConfig.PORT_465 if EMAIL_USE_SSL else EmailConfig.PORT_587
+EMAIL_HOST_USER = EmailConfig.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EmailConfig.EMAIL_HOST_PASSWORD
+
+# Celery Configuration
+CELERY_BROKER_URL = CeleryConfig.CELERY_BROKER_URL
+CELERY_TIMEZONE = Settings.TIME_ZONE
+CELERY_RESULT_BACKEND = "redis"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_EXTENDED = True
+
+# Logging Configuration
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "debug.log",
+            "formatter": "verbose",
+        },
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+    },
+    "loggers": {
+        "rewards": {
+            "handlers": ["file", "console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+}
---
+++ utils/constants.py
@@ -0,0 +1,42 @@
+# Django Admin Bulk i/o Constants
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+DJANGO_DATABASE_URL = config["DATABASE_URL"]
+
+
+class Settings:
+    """Settings Constants"""
+
+    SECRET_KEY = config["SECRET_KEY"]
+    DATABASE_URL = config["DATABASE_URL"]
+    ROOT_URLCONF = "django_admin_bulk_io.urls"
+    TEMPLATES = "templates"
+    WSGI_APPLICATION = "django_admin_bulk_io.wsgi.application"
+    STATIC_URL = "/static/"
+    STATIC_FILES_DIRS = "templates/static/"
+    STATIC_ROOT = "assets/"
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = "media"
+    LANGUAGE_CODE = "en-us"
+    TIME_ZONE = "UTC"
+    USE_I18N = True
+    USE_TZ = True
+
+
+class EmailConfig:
+    """Email Constants"""
+
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "smtp.gmail.com"
+    PORT_587 = 587
+    PORT_465 = 465
+    EMAIL_HOST_USER = config["EMAIL_HOST_USER"]
+    EMAIL_HOST_PASSWORD = config["EMAIL_HOST_PASSWORD"]
+
+
+class CeleryConfig:
+    """Celery Config"""
+
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
